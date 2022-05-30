248cdd53b9790caff831097241e62d96e1122689
Matts Blade 15 <matt.graham@bellevuecollege.edu>
2022-05-30 16:48:30 -0700

    start

---
+++ ott_api.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+class Probability:
+    print(f"Calculate probabilities here")
+
+    class Poisson():
+        print(f"Poisson")
+
+        def get_prob():
+            print(f"Get probability")
+
+    class Normal():
+        print(f"Normal")
+
+        def get_prob():
+            print(f"Get probability")
+
+            
+class Tests:
+    def t_test():
+        print(f"T test")
+
+    def z_test():
+        print(f"Z test")
