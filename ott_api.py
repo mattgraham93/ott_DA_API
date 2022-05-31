2d94d39d744ef8b85e9deea6cc6c162c24a287a7
Matt Graham <matt.graham@bellevuecollege.edu>
2022-05-30 20:18:20 -0700

    working on poisson

--- ott_api.py
+++ ott_api.py
@@ -1,24 +1,39 @@
 import pandas as pd
+import scipy.stats as st
 
 class Probability:
-    print(f"Calculate probabilities here")
-
     class Poisson():
-        print(f"Poisson")
+        def __init__(self, name: str) -> object:
+            print(f"Making distribution")
+            self.name = name
+            self.dist = st.poisson.rvs(mu=0.6, size=1000)  # getting random poisson distribution
+            print(f"Created")
 
-        def get_prob():
+        def get_prob(sel: float, mu: float):
             print(f"Get probability")
+            return st.poisson.pmf(k, mu)
 
-    class Normal():
-        print(f"Normal")
+#     class Normal():
+#         print(f"Normal")
 
-        def get_prob():
-            print(f"Get probability")
+#         def get_prob():
+#             print(f"Get probability")
 
             
-class Tests:
-    def t_test():
-        print(f"T test")
+# class Tests:
+#     def t_test():
+#         print(f"T test")
 
-    def z_test():
-        print(f"Z test")
+#     def z_test():
+#         print(f"Z test")
+
+
+def main():
+    pois = Probability.Poisson(name="Poisson")
+    print(pois.dist)  # this is the acual distribution, a list
+
+    pois.get_prob(3, 4)
+
+
+if __name__ == "__main__":
+    main()
