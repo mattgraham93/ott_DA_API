530f06772067f7f47a63f07e77dddbeec4ff5758
Matt Graham <matt.graham@bellevuecollege.edu>
2022-06-08 18:46:55 -0700

    cont work

--- ott_api.py
+++ ott_api.py
@@ -3,21 +3,26 @@ import scipy.stats as st
 
 class Probability:
     class Poisson():
-        def __init__(self, name: str) -> object:
-            print(f"Making distribution")
-            self.name = name
-            self.dist = st.poisson.rvs(mu=0.6, size=1000)  # getting random poisson distribution
-            print(f"Created")
+        # def __init__(self, name: str) -> object:
+        #     print(f"Making distribution")
+        #     self.name = name
+        #     self.dist = s
+        #     print(f"Created")
+
+        def get_dist(self, mu, n):
+            return st.poisson.rvs(mu=mu, size=n)  # getting random poisson distribution
+
+        def get_prob(sel: float, mu: float):
+            # print(f"Get probability")
+            return st.poisson.pmf(sel, mu)
+
+    class Normal():
+        def get_dist(self, ):
+            return st.norm.rvs(mu=0.6, size=1000)  # getting random poisson distribution
 
         def get_prob(sel: float, mu: float):
             print(f"Get probability")
-            return st.poisson.pmf(k, mu)
-
-#     class Normal():
-#         print(f"Normal")
-
-#         def get_prob():
-#             print(f"Get probability")
+            return st.norm.pmf(sel, mu)
 
             
 # class Tests:
@@ -29,10 +34,10 @@ class Probability:
 
 
 def main():
-    pois = Probability.Poisson(name="Poisson")
-    print(pois.dist)  # this is the acual distribution, a list
-
-    pois.get_prob(3, 4)
+    pois_dist = Probability.Poisson().get_dist(5, 1000)
+    print(pois_dist)  # this is the acual distribution, a list
+    my_prob = Probability.Poisson.get_prob(sel = 3, mu = 4)
+    print(my_prob)  # get probability of selection and expected mean
 
 
 if __name__ == "__main__":
